6b7d5a045d62dfc6ed7436252eec0fae5aeaf7aa
LeslieAmaya <leslieamayal001@gmail.com>
2024-06-04 22:01:09 -0700

    formulario empezado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { NavComponent } from './nav/nav.component';
 import { MiComponent } from './mi/mi.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
     {path:'inicio', component: PrincipalComponent},
     {path:'mi', component: MiComponent},
+    {path:'contacto', component: ContactoComponent},
     {path:'proyectos', component:ProyectosComponent},
     {path:'nav', component:NavComponent}
 ];
